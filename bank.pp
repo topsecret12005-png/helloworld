e72a80e95bc02844ed80a92f65764179a9dc2b71
ANDUPANDU <topsecret12005@gmail.com>
2026-04-27 12:29:13 +0530

    Update bank.pp

--- bank.pp
+++ bank.pp
@@ -28,49 +28,46 @@ int main() {
     for(j = 0; j < m; j++)
         cin >> avail[j];
 
-    // 1. Calculate NEED Matrix (Need = Max - Alloc) & Initialize Finish to 0
     for(i = 0; i < n; i++) {
         for(j = 0; j < m; j++) {
             need[i][j] = max[i][j] - alloc[i][j];
         }
-        finish[i] = 0; // Assume no process is finished yet
+        finish[i] = 0; 
     }
 
-    // 2. The Safety Check
-    for(k = 0; k < n; k++) {          // Repeat 'n' times to ensure all processes get checked
+    for(k = 0; k < n; k++) {         
         for(i = 0; i < n; i++) {
-            if(finish[i] == 0) {      // Only check unfinished processes
-                flag = 0;             // Assume process CAN run
+            if(finish[i] == 0) {      
+                flag = 0;            
                 
                 for(j = 0; j < m; j++) {
                     if(need[i][j] > avail[j]) {
-                        flag = 1;     // It needs more than we have! It CANNOT run.
+                        flag = 1;     
                         break;
                     }
                 }
 
-                // If the flag is still 0, the process CAN run safely
                 if(flag == 0) {
-                    safeSeq[count++] = i;         // Add to safe sequence
+                    safeSeq[count++] = i;         
                     for(j = 0; j < m; j++) {
-                        avail[j] += alloc[i][j];  // Return its resources to the bank
+                        avail[j] += alloc[i][j];  
                     }
-                    finish[i] = 1;                // Mark as finished
+                    finish[i] = 1;               
                 }
             }
         }
     }
 
-    // 3. Final Verification
+    
     is_safe = 1;
     for(i = 0; i < n; i++) {
         if(finish[i] == 0) {
-            is_safe = 0; // Found a process that couldn't finish
+                is_safe = 0;                                                                                                         
             break;
         }
     }
 
-    // Output the Result
+   
     if(is_safe == 1) {
         cout << "\n[SUCCESS] System is in SAFE state.\nSafe Sequence: ";
         for(i = 0; i < n; i++) {
